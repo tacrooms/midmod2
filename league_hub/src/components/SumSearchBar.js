2a9d500222044672e56c05e668b8515a09909636
tacrooms <87332211+tacrooms@users.noreply.github.com>
2021-09-15 10:30:16 -0400

    search bar now working

--- league_hub/src/components/SumSearchBar.js
+++ league_hub/src/components/SumSearchBar.js
@@ -1,21 +1,49 @@
 import React, {useState} from 'react'
+import axios from 'axios'
+
+function SumSearchBar() {
+    const [SummonerName, setSummonerName] = useState('');
+    
+
+    const handleChange = (e) => {
+        setSummonerName(e.target.value)
+    }
+
+    // const handleCLick = () => {
+    
+    //     console.log('success')
+    // }
+
+    const handleSubmit = async(e) => {
 
 
-const App = () => {
+        e.preventDefault()
+        try {
+            const response = await axios.get(`https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${SummonerName}?api_key=RGAPI-55b32194-5947-4592-b4ba-bffcae5b82e3`)
+            console.log(response.data)
+        } catch (error) {
+            console.log('error')
+            
+        }
 
-    const [summonerName, setSummonerName] = useState('');
-    const []
+
+    }
+
+    
+
+
 
 
 
     return (
         <div>
-                <form>
-                <input placeholder= "Enter Summoner Name" />
-                <button type="submit">Submit</button>
+                <form onSubmit={(e)=>{return handleSubmit(e)}}>
+                <input onChange={(e)=>{return handleChange(e)}} type='text' placeholder= "Enter Summoner Name" value={SummonerName} />
+                <button  type="submit">Submit</button>
             </form>
         </div>
     )
 }
 
 export default SumSearchBar
+
